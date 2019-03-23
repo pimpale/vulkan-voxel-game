f3b7aa643732a8ec67a7de794a5c0b8e5e5ebd0e
pimpale <gpimpale29@gmail.com>
2019-03-23 11:32:48 -0700

    renamed shaders

---
+++ src/shader/gridupdategrid.rs
@@ -0,0 +1,31 @@
+vulkano_shaders::shader! {
+    ty: "compute",
+    src: "
+#version 450
+
+struct Node {
+    uint leftChildIndex;
+    uint rightChildIndex;
+    uint parentIndex;
+    uint age;
+    uint archetypeId;
+    uint status;
+    bool visible;
+    float area;
+    float length;
+    vec4 absolutePositionCache;
+    mat4 transformation;
+};
+
+layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+
+layout(set = 0, binding = 0) buffer NodeBuffer {
+    Node nodes[];
+} buf;
+
+void main() {
+    uint idx = gl_GlobalInvocationID.x;
+    buf.nodes[idx].age++;
+}
+"
+}
